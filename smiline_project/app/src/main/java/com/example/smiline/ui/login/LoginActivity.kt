5629e9b0e84015f3987bbcd4851e3c94e43d45fd
unkonown <b1021091@fun.ac.jp>
2021-12-12 22:11:05 +0900

    add: firebase.json

--- smiline_project/app/src/main/java/com/example/smiline/ui/login/LoginActivity.kt
+++ smiline_project/app/src/main/java/com/example/smiline/ui/login/LoginActivity.kt
@@ -1,15 +1,24 @@
 package com.example.smiline.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.smiline.MainActivity
 import com.example.smiline.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val useridEditText=findViewById<android.widget.EditText>(R.id.userid)
         val passwordEditText=findViewById<android.widget.EditText>(R.id.password)
+        findViewById<Button>(R.id.login_button).setOnClickListener(this)
+    }
 
+    override fun onClick(view: View) {
+        val intent=Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
     }
 }
