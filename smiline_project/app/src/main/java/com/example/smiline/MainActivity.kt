4b44019de8410cd0b4fc7849b3dd3ce327147674
unkonown <b1021091@fun.ac.jp>
2021-12-14 17:32:35 +0900

    fix:Course class typo

--- smiline_project/app/src/main/java/com/example/smiline/MainActivity.kt
+++ smiline_project/app/src/main/java/com/example/smiline/MainActivity.kt
@@ -7,17 +7,33 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.room.Room
 import com.example.smiline.databinding.ActivityMainBinding
+import com.example.smiline.model.db.AppDatabase
+import com.example.smiline.model.db.Course
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var courses : List<Course> = listOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        GlobalScope.launch {
+            val db: AppDatabase = Room
+                .databaseBuilder(
+                    applicationContext,
+                    AppDatabase::class.java, "database-name"
+                ).fallbackToDestructiveMigration()
+                .build()
+            courses = db.userDao().getAll()
+            println(courses)
+        }
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
