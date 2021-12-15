cd427b896089dc19b228d7d7aa31f84c3ba4a6e4
muramasa <muramasa.kyojin18@outlook.jp>
2021-12-16 00:06:32 +0900

    Login画面です!

--- smiline_project/app/src/main/java/com/example/smiline/ui/chat/ChatActivity.kt
+++ smiline_project/app/src/main/java/com/example/smiline/ui/chat/ChatActivity.kt
@@ -2,6 +2,7 @@ package com.example.smiline.ui.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smiline.R
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
