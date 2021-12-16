274164316ae1861dbca0ebd78f1f0658a9fcbfb7
unkonown <b1021091@fun.ac.jp>
2021-12-17 06:19:59 +0900

    add:chat logic

--- smiline_project/app/src/main/java/com/example/smiline/ui/chat/ChatActivity.kt
+++ smiline_project/app/src/main/java/com/example/smiline/ui/chat/ChatActivity.kt
@@ -1,17 +1,21 @@
 package com.example.smiline.ui.chat
 
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toast
+import android.util.Log
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.example.smiline.R
 import com.example.smiline.model.db.AppDatabase
+import com.example.smiline.util.data.Chat
+import com.example.smiline.util.ui.listAdapter.ChatAdapter
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -19,6 +23,8 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.GlobalScope
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 
 class ChatActivity : AppCompatActivity() {
@@ -26,15 +32,37 @@ class ChatActivity : AppCompatActivity() {
     private var imageUri: Uri?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*firebase*/
-        val firestore: FirebaseFirestore = Firebase.firestore
-        val storage = Firebase.storage
-        val storageRef = storage.reference
-        /*firebase*/
         /*courseId*/
         val courseId=intent.getIntExtra("id",0)
         val courseName=intent.getStringExtra("name")
         /*courseId*/
+        supportActionBar?.title=courseName
+        /*firebase*/
+        val firebaseAuth= Firebase.auth
+        val firestore: FirebaseFirestore = Firebase.firestore
+        val collectionRef=firestore.collection(courseId.toString()).orderBy("timestamp").addSnapshotListener{ snapshot, e ->
+            if (snapshot != null) {
+                var chatList=ArrayList<Chat>()
+                snapshot.documents.forEach {
+                    println(it.data)
+                    val chat=Chat(it.getString("content")!!,firebaseAuth.currentUser?.photoUrl.toString(),
+                        it.getString("name")!!,it.getString("uri"))
+                    chatList.add(chat)
+                }
+                /*View*/
+                if(chatList.size!=0){
+                    println(chatList.size)
+                    Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
+                    val listView=findViewById<ListView>(R.id.chat_list)
+                    listView.adapter= ChatAdapter(this,chatList!!)
+                }
+                /*View*/
+            }
+        }
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        /*firebase*/
+
         if(savedInstanceState != null){
             imageUri = savedInstanceState.getParcelable("imageUri")
             val imageView = findViewById<ImageView>(R.id.messageImage)
@@ -51,13 +79,30 @@ class ChatActivity : AppCompatActivity() {
             startActivityForResult(intent, READ_REQUEST_CODE)
         }
         sendButton.setOnClickListener {
-            val massageId=firestore.collection(courseId.toString()).id
 
             if(imageUri!=null){
                 //画像をアップロード
+                /*
+                //test bitmap
                 storageRef.child("image.png")
+                val imageView = findViewById<ImageView>(R.id.messageImage)
+                val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+                val baos = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                val data = baos.toByteArray()
+                var uploadTask = storageRef.putBytes(data)
+                uploadTask.addOnFailureListener {
+                    // Handle unsuccessful uploads
+                }.addOnSuccessListener { taskSnapshot ->
+                    // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                    // ...
+                }
+                //test bitmap
+                 */
+                val uuid = UUID.randomUUID().toString()
+                val ref=storageRef.child("massage/${courseId.toString()}/${uuid}.jpg")
                 findViewById<ImageView>(R.id.messageImage).setImageDrawable(null)
-                val uploadTask = storageRef.putFile(imageUri!!)
+                val uploadTask = ref.putFile(imageUri!!)
                 println(imageUri!!.toString())
                 val urlTask = uploadTask.continueWithTask { task ->
                     if (!task.isSuccessful) {
@@ -65,23 +110,25 @@ class ChatActivity : AppCompatActivity() {
                             throw it
                         }
                     }
-                    storageRef.downloadUrl
+                    ref.downloadUrl
                 }.addOnCompleteListener { task ->
                     imageUri=null
                     if (task.isSuccessful) {
                         val downloadUri = task.result
-                        sendMessage(courseId,downloadUri,firestore,massageId)
-                        println("URL:$downloadUri")
+                        println("done")
+                        sendMessage(courseId,task.result,firestore)
                     } else {
                         // Handle failures
                         // ...
-                        //println(task.exception)
-                        Toast.makeText(this, "エラー", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, "aaaaaa", Toast.LENGTH_SHORT).show()
                     }
+                    //val downloadUri =
+                    //sendMessage(courseId,downloadUri,firestore,massageId)
+                    //println("URL:$downloadUri")
                 }
             }
             else{
-                sendMessage(courseId,null,firestore,massageId)
+                sendMessage(courseId,null,firestore)
             }
         }
     }
@@ -104,6 +151,7 @@ class ChatActivity : AppCompatActivity() {
                         imageView.setImageBitmap(image)
                     }
                 } catch (e: Exception) {
+                    println(e.toString())
                     Toast.makeText(this, "エラーが発生しました", Toast.LENGTH_LONG).show()
                 }
             }
@@ -116,24 +164,25 @@ class ChatActivity : AppCompatActivity() {
         //savedInstanceState.putBoolean("Num", mNum)
     }
 
-    fun sendMessage(courseId:Int,uri:Uri?,firestore: FirebaseFirestore,id:String){
+    fun sendMessage(courseId:Int,uri:Uri?,firestore: FirebaseFirestore){
         val editText=findViewById<EditText>(R.id.messageEditText)
         val messageText=editText.text.toString()
-        //val firebaseAuth= Firebase.auth
-        //var userName=firebaseAuth.currentUser?.displayName
-        /*
+        val firebaseAuth= Firebase.auth
+        var userName=firebaseAuth.currentUser?.displayName
+
         if(userName==null || userName.isEmpty()){
             userName="名無し"
-        }*/
+        }
         val messageData = hashMapOf(
-            "name" to "korean",
+            "name" to userName,
             "content" to messageText,
-            "uri" to uri,
-            "id" to id)
+            "uri" to uri.toString(),
+            "timestamp" to Timestamp.now())
+
         firestore.collection(courseId.toString())
             .add(messageData)
             .addOnSuccessListener { documentReference ->
-                Toast.makeText(this, "メッセージを送信しました", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "メッセージを送信しました", Toast.LENGTH_SHORT).show()
                 editText.text.clear()
             }
             .addOnFailureListener { e ->
--- smiline_project/app/src/main/java/com/example/smiline/util/ui/listAdapter/listAdapter.kt
+++ smiline_project/app/src/main/java/com/example/smiline/util/ui/listAdapter/listAdapter.kt
@@ -2,14 +2,19 @@ package com.example.smiline.util.ui.listAdapter
 
 import android.content.Context
 import android.content.ContextParams
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.smiline.R
 import com.example.smiline.model.db.Course
 import com.example.smiline.ui.home.HomeFragment
+import com.example.smiline.util.data.Chat
+import com.squareup.picasso.Picasso
+import java.util.ArrayList
 
 class ListAdapter(val context: Context, val Courses: List<Course>) : BaseAdapter() {
 
@@ -34,5 +39,42 @@ class ListAdapter(val context: Context, val Courses: List<Course>) : BaseAdapter
 
     override fun getCount(): Int {
         return Courses.size
+    }
+}
+
+class ChatAdapter(val context: Context, val Chats: ArrayList<Chat>) : BaseAdapter() {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val view: View = LayoutInflater.from(this.context).inflate(R.layout.chat_item, null)
+        val icon= view.findViewById<ImageView>(R.id.icon)
+        val content=view.findViewById<TextView>(R.id.content)
+        val username=view.findViewById<TextView>(R.id.username)
+        val image=view.findViewById<ImageView>(R.id.contentImage)
+
+
+        val chat = Chats[position]
+        println(chat.iconUrl)
+        if(chat.iconUrl!="null"){
+            Picasso.get().load(chat.iconUrl).into(icon)
+        }
+        else{
+            icon.setImageResource(R.drawable.ic_baseline_cruelty_free_24)
+        }
+        content.text=chat.content
+        username.text=chat.username
+        Picasso.get().load(chat.imageUrl).into(image)
+        return view
+    }
+
+    override fun getItem(position: Int): Any {
+        return Chats[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getCount(): Int {
+        return Chats.size
     }
 }
